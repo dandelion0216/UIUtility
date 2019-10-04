74d8af500c26e4d724949993e27a7da8e5c49c3d
nobuharu.kusama <nobuharu.kusama@panoam.com>
2019-10-04 19:16:13 +0900

    feat: first commit
    add workspace
    add NKCurrentViewController protocol

---
+++ UIUtility/UIUtility.h
@@ -0,0 +1,19 @@
+//
+//  UIUtility.h
+//  UIUtility
+//
+//  Created by 草間寛啓 on 2019/10/04.
+//  Copyright © 2019 nobuharu kusama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIUtility.
+FOUNDATION_EXPORT double UIUtilityVersionNumber;
+
+//! Project version string for UIUtility.
+FOUNDATION_EXPORT const unsigned char UIUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIUtility/PublicHeader.h>
+
+
---
+++ UIUtility/UIViewController/NKCurrentViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NKCurrentViewController.swift
+//  UIUtility
+//
+//  Created by 草間寛啓 on 2019/10/04.
+//  Copyright © 2019 nobuharu kusama. All rights reserved.
+//
+
+import UIKit
+
+public protocol NKCurrentViewController where Self: UIViewController {
+    var currentViewController: UIViewController? { get }
+    func findCurrentViewController(viewController vc: UIViewController?) -> UIViewController?
+}
+
+
+extension NKCurrentViewController {
+    
+    var currentViewController: UIViewController? {
+        return nil
+    }
+    func findCurrentViewController(viewController vc: UIViewController?) -> UIViewController? {
+        guard let base: UIViewController = vc else {
+            return nil
+        }
+        switch base {
+        case is UINavigationController:
+            return self.findCurrentViewController(viewController: (base as! UINavigationController).topViewController)
+        case is UITabBarController:
+            return self.findCurrentViewController(viewController: (base as! UITabBarController).selectedViewController)
+        default:
+            break
+        }
+        if let presented: UIViewController = base.presentedViewController {
+            return self.findCurrentViewController(viewController: presented)
+        }
+        return base
+    }
+}
